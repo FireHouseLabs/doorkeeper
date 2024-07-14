08150dcfd8c992bfdf523cace8db50818e86d2a5
jeremyw24 <jeremy@visionswest.com.au>
2024-07-14 12:57:39 +1000

    feat: add logging

--- src/routes/control/+page.server.ts
+++ src/routes/control/+page.server.ts
@@ -2,6 +2,7 @@ import { redirect, type Actions, type RequestEvent } from '@sveltejs/kit';
 import { authenticate, getToken } from '$lib/services/inceptionAuthService';
 import type { PageServerLoad } from '../$types';
 import { PRIVATE_INCEPTION_BASE_URL } from '$env/static/private';
+import { supabase } from '$lib/supabaseClient';
 
 export const load = (async ({ locals: { getSession } }) => {
 	const session = await getSession();
@@ -47,23 +48,60 @@ async function controlDoor(id: string, token: string) {
   }
   
   export const actions: Actions = {
-    control: async ({ request }: RequestEvent) => {
+    control: async ({ request, locals: { getSession } }: RequestEvent) => {
+      const session = await getSession();
+      if (!session) {
+        throw redirect(302, '/login');
+      }
+  
       const formData = await request.formData();
       const doorId = formData.get('doorId') as string;
+      const doorName = formData.get('doorName') as string; // Ensure the door name is also sent
   
-      // Get token or authenticate
       let token = getToken();
       if (!token) {
         token = await authenticate();
       }
   
-      // Ensure token is a string before passing it to controlDoor
       if (token) {
         try {
           const result = await controlDoor(doorId, token);
+  
+          // Insert a record into the Supabase entry_logs table
+          const { error } = await supabase
+            .from('entry_logs')
+            .insert({
+              user: session.user.id,
+              user_email: session.user.email,
+              door_name: doorName,
+              door_id: doorId,
+              status: true // Mark as successful
+            });
+  
+          if (error) {
+            console.error('Error inserting entry log:', error);
+            return { success: false, error: 'Failed to log entry' };
+          }
+  
           return { success: true, result };
         } catch (error) {
           console.error('Failed to execute control action:', error);
+  
+          // Insert a record into the Supabase entry_logs table with status false
+          const { error: logError } = await supabase
+            .from('entry_logs')
+            .insert({
+              user: session.user.id,
+              user_email: session.user.email,
+              door_name: doorName,
+              door_id: doorId,
+              status: false // Mark as unsuccessful
+            });
+  
+          if (logError) {
+            console.error('Error logging failed entry attempt:', logError);
+          }
+  
           return { success: false, error: (error as Error).message };
         }
       } else {
