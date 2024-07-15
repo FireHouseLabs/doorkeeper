b6bbc832945f4c202cc1abf218d0e1ddd0a3bddd
jeremyw24 <jeremy@visionswest.com.au>
2024-07-15 16:37:31 +1000

    another attempt

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,8 +1,10 @@
-const tailwindcss = 'tailwindcss';
-const autoprefixer = 'autoprefixer';
+const tailwindcss = require('tailwindcss');
+const autoprefixer = require('autoprefixer');
 
 const config = {
 	plugins: [
+		require('postcss-import'),
+		require('@tailwindcss/nesting'),
 		//Some plugins, like tailwindcss/nesting, need to run before Tailwind,
 		tailwindcss(),
 		//But others, like autoprefixer, need to run after,
