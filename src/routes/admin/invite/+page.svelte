a000417966b52f5a179ad09f62a19c3b8ee10442
jeremyw24 <jeremy@visionswest.com.au>
2024-07-15 14:21:10 +1000

    fix: import bars icon

--- src/routes/admin/invite/+page.svelte
+++ src/routes/admin/invite/+page.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
 	import { enhance } from '$app/forms';
 	import type { ActionData, SubmitFunction } from './$types.js';
+	import BarsRotateIcon from '$lib/icons/BarsRotateIcon.svelte';
 
 	export let form: ActionData;
 
